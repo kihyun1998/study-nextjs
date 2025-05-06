886376702b3267aa0927778c90dcc76cafb00eae
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2025-05-06 18:18:05 +0900

    data fetch 중

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,10 +1,22 @@
+import { API_URL } from '../../../(home)/page';
 
 export const metadata = {
-    title: 'Movies',
+  title: 'Movies',
+};
+
+async function getMovie(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const res = await fetch(`${API_URL}/${id}`);
+  return res.json();
 }
 
-export default function MovieDetail({params: {id}} : {params: {id :string}}){
-    return (
-        <h1>Movie {id}</h1>
-    );
+export default async function MovieDetail(props: { params: { id: string } }) {
+  // params를 먼저 await로 기다립니다
+  const params = await props.params;
+  const id = params.id;
+
+  console.log(id);
+  const movie = await getMovie(id);
+  return <h1>{movie.title}</h1>;
 }
