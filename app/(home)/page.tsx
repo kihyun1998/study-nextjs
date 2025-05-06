426e35c047e1c64a40bac6ae456daf8382d96ecd
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2025-05-06 17:01:26 +0900

    study data fetch use cs

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,27 @@
-export const metadata = {
-    title: 'Home',
-}
+"use client";
 
-export default function Tomato(){
+import { useEffect, useState } from "react";
+
+// export const metadata = {
+//     title: 'Home',
+// }
+
+export default function HomePage(){
+    const [isLoading, setIsLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async()=>{
+        const res = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
+        const json =  await res.json();
+        setMovies(json);
+        setIsLoading(false);
+    }
+    useEffect(()=>{
+        getMovies();
+    },[]);
     return (
         <div>
             <h1>Home</h1>
+           {isLoading ? "Loading..." : JSON.stringify(movies)}
         </div>
     );
 }
