b0ac3b471f742cd48baa3053a3556f88237d7a38
박기현 <kihyun1998@penta.co.kr>
2025-05-06 23:05:54 +0900

    study promise all

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -11,12 +11,17 @@ async function getMovie(id: string) {
   return res.json();
 }
 
+async function getVideos(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const res = await fetch(`${API_URL}/${id}/videos`);
+  return res.json();
+}
+
 export default async function MovieDetail(props: { params: { id: string } }) {
-  // params를 먼저 await로 기다립니다
   const params = await props.params;
   const id = params.id;
 
-  console.log(id);
-  const movie = await getMovie(id);
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
   return <h1>{movie.title}</h1>;
 }
