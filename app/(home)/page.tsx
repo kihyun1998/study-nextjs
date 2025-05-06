f37afa6bcb3f9968297ed6006036cb96baf5399b
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2025-05-06 17:16:16 +0900

    study data fetching in ssr

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,27 +1,22 @@
-"use client";
 
-import { useEffect, useState } from "react";
+export const metadata = {
+    title: 'Home',
+}
 
-// export const metadata = {
-//     title: 'Home',
-// }
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+async function getMovies() {
+    await new Promise((resolve)=> setTimeout(resolve,5000));
+    const res = await fetch(URL);
+    const json = await res.json();
+    return json;
+}
 
-export default function HomePage(){
-    const [isLoading, setIsLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async()=>{
-        const res = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
-        const json =  await res.json();
-        setMovies(json);
-        setIsLoading(false);
-    }
-    useEffect(()=>{
-        getMovies();
-    },[]);
+export default async function HomePage(){
+    const movies = await getMovies();
     return (
         <div>
             <h1>Home</h1>
-           {isLoading ? "Loading..." : JSON.stringify(movies)}
+           {JSON.stringify(movies)}
         </div>
     );
 }
