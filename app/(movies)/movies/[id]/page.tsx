1ecaafa7c1147f7d9b0e80eb867dcbfaff7adc14
박기현 <kihyun1998@penta.co.kr>
2025-05-07 00:50:53 +0900

    dynamic metadata

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,19 +1,26 @@
 import { Suspense } from 'react';
-import MovieInfo from '../../../../components/movie-info';
+import MovieInfo, { getMovie } from '../../../../components/movie-info';
 import MovieVideos from '../../../../components/movie-videos';
 
-export const metadata = {
-  title: 'Movies',
-};
+interface IParams {
+  params: { id: string };
+}
 
-export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetail({ params: { id } }: IParams) {
   return (
     <div>
-      <Suspense>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
 
-      <Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
         <MovieVideos id={id} />
       </Suspense>
     </div>
