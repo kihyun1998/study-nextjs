b03effd7d8c5b1b52667c4301ba80bf08c56a325
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2025-05-06 16:09:15 +0900

    stduy layout

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,9 +1,7 @@
-import Nav from "../../components/navigation";
 
 export default function AboutUs(){
 return (
         <div>
-            <Nav />
             <h1>About us</h1>
         </div>
     );}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Nav from "../components/navigation";
 
 export default function Tomato(){
     return (
         <div>
-            <Nav />
             <h1>Home</h1>
         </div>
     );
